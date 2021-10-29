ce53449b221adc2a7dc8be3cdee3dd508e95ab26
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-10-29 12:24:08 -0500

    m0.4 Add proyecto visual

---
+++ ChessMax/AccesoDatos/conexion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace AccesoDatos
+{
+    class conexion
+    {
+        MySqlConnection _conn;
+        public conexion(string server, string user, string password, string based, uint port)
+        {
+            _conn = new MySqlConnection(string.Format("server={0}; user={1}; password={2}; database={3};" +
+                "port={4}", server, user, password, based, port));
+        }
+        public void Consultar(string consulta)
+        {
+            try
+            {
+                MySqlCommand command = new MySqlCommand(consulta, _conn);
+                _conn.Open();
+                command.ExecuteNonQuery();
+                _conn.Close();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public string ConsultaRetorno(string consulta)
+        {
+            string r;
+            try
+            {
+                _conn.Open();
+
+                var command = new MySqlCommand(consulta, _conn);
+                command.ExecuteNonQuery();
+                r = Convert.ToString(command.ExecuteScalar());
+
+                _conn.Close();
+                return r;
+            }
+            catch (Exception)
+            {
+
+                return "ERROR";
+            }
+        }
+
+        public DataSet Mostrar(string tabla, string consulta)
+        {
+            var ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(consulta, _conn);
+                _conn.Open();
+                da.Fill(ds, tabla);
+                _conn.Close();
+                return ds;
+            }
+            catch (Exception)
+            {
+                _conn.Close();
+                return ds;
+            }
+        }
+        public DataSet ObtenerDatos(string consulta, string tabla)
+        {
+            var ds = new DataSet();
+            MySqlDataAdapter da = new MySqlDataAdapter(consulta, _conn);
+            da.Fill(ds, tabla);
+
+            return ds;
+        }
+
+    }
+}
