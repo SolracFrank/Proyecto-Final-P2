7ba8ded4fee2faa6026bac0a94e79bc01d4185f5
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-11-09 10:37:42 -0600

    Campeonatos Anteriores Entidades

---
+++ ChessMax/AccesoDatos/EntidadCampeonatosAnteriores.cs
@@ -0,0 +1,17 @@
+﻿namespace AccesoDatos
+{
+    public class EntidadCampeonatosAnteriores
+    {
+        public EntidadCampeonatosAnteriores(string fkParticipante, string nombre, string tipo_Participacion)
+        {
+            FkParticipante = fkParticipante;
+            Nombre = nombre;
+            Tipo_Participacion = tipo_Participacion;
+        }
+        public string FkParticipante { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo_Participacion { get; set; }
+
+
+    }
+}
