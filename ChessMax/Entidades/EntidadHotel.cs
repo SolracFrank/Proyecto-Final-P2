632b0b4e5f8b1654217e796d7b4411d3eac7813d
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-11-09 10:45:02 -0600

    Pre 1

---
+++ ChessMax/Entidades/EntidadHotel.cs
@@ -0,0 +1,13 @@
+﻿namespace Entidades
+{
+    public class EntidadHotel
+    {
+        public string NombreHotel { get; set; }
+        public string Ciudad { get; set; }
+        public string Colonia { get; set; }
+        public string Calle { get; set; }
+        public int Numero { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
