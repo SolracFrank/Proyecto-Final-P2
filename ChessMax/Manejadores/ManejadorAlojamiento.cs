c50a22e81e85fb8298366ccb822c6fa8c5c6f1b6
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-11-15 09:54:04 -0600

    Añadiendo metodos alojamiento

--- ChessMax/Manejadores/ManejadorAlojamiento.cs
+++ ChessMax/Manejadores/ManejadorAlojamiento.cs
@@ -16,6 +16,21 @@ namespace Manejadores
             _base = new conexion("localhost", "root", "", "clubajedrez", 3306);
         }
 
+        public void AddAlojamiento(EntidadAlojamiento alojar)
+        {
+            _base.Consultar(string.Format("CALL p_addAlojamiento('{0}','{1}','{2}','{3}');", alojar.Codigo, alojar.Fecha, alojar.FkHotel, alojar.FkParticipante));
+        }
+        public void DelAlojamiento(EntidadAlojamiento alojar)
+        {
+            _base.Consultar(string.Format("CALL p_delAlojamiento('{0}');", alojar.Codigo));
+        }
+        public void updateAlojamiento(EntidadAlojamiento alojar)
+        {
+            _base.Consultar(string.Format("CALL p_updateAlojamiento('{0}','{1}','{2}','{3}');", alojar.Codigo, alojar.Fecha, alojar.FkHotel, alojar.FkParticipante));
+        }
+        public void MostrarAlojamiento(DataGridView tabla, string dato)
+        {
 
+        }
     }
 }
